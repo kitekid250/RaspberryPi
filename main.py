82c19ebf8a8ce6f27a08ab451a04f7a0b1df457c
Ward Prescott <wardprescott95@gmail.com>
2014-05-29 17:05:39 -0400

    Final Implementation of Daily Checker in Main

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 ### Made by Ward Prescott Summer 2014
 ### Built for Python 3.4 with support for Python 2.7
-
+# Ryan was here
 import datetime
 
-#Function to check if the LMP data has been taken for the day
-file = file.open("LMPLog.txt")
+def importLMP():
+	'''This function checks to see if LMP data has been downloaded for the day, and if it has not, it uses the file LMP.py to download it.
+	'''
+
+	file = open("LMPLog.txt", 'r')
+	logText = file.read()
+	file.close()
+
+	if logText[0:10] != str(datetime.date.today()):
+		print("Data not taken today.  Downloading data from PJM.")
+		import lmp
+		file = open("LMPLog.txt", 'w')
+		file.write(str(datetime.date.today()) + logText[0:100])
+		file.close()
+
+importLMP()
